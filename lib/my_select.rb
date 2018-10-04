9353a45cb5491c52bd13f3efd461edc702fcedf2
Emma Gouti <goutiemma@gmail.com>
2018-10-04 03:25:46 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,8 @@ def my_select(collection)
   i = 0
   arr =[]
   while i < collection.length
-  if yield(collection[i])
+  if yield(collection[i]) == true 
+    arr << collection[i]
   i += 1 
 end
 return arr
