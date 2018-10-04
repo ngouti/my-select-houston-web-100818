3b9f30a6039eb19588322eba33aec4d672c6a0e0
Emma Gouti <goutiemma@gmail.com>
2018-10-04 03:21:35 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,14 +1,9 @@
 def my_select(collection)
-  if block_given?
-  i = 0
-  arr =[]
-  while i < collection.length
-  arr << yield(collection[i])
-  i += 1 
+  result =[]
+    self.each do |element|
+      result << element if block.call(element) == true
+    end
+    result
 end
-return arr
-else
-  puts "No block"
-end
-end
+
 
